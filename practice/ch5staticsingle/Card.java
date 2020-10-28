9726dad8a438a59dd547355312f7c960024d4f27
hws522 <sraphic@kakao.com>
2020-10-28 17:22:55 +0900

    finish

--- practice/ch5staticsingle/Card.java
+++ practice/ch5staticsingle/Card.java
@@ -2,4 +2,24 @@ package practice.ch5staticsingle;
 
 public class Card {
     
+    private int cardNumber;
+    public static int serialNum = 10000;
+
+    public Card()
+    {
+        serialNum++;
+        cardNumber = serialNum;
+    }
+
+    public int getCardNumber()
+    {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber)
+    {
+        this.cardNumber = cardNumber;
+    }
+
+
 }
--- practice/ch5staticsingle/CardCompany.java
+++ practice/ch5staticsingle/CardCompany.java
@@ -1,8 +1,23 @@
 package practice.ch5staticsingle;
 /*
 카드회사가 있다. 카드회사는 유일한 객체이고, 이 회사에서는 카드를 발급하면
-항상 고유번호가 자동으로 생성된다. 10001qnxj tlwkrgkdu zkemrk todtjdehlf Eoakek 10002, 10003 식으로 증가 된다.
+항상 고유번호가 자동으로 생성된다. 
+10001부터 시작하여 카드가 생성될 때마다 10002, 10003 식으로 증가 된다.
 */
 public class CardCompany {
     
+    private static CardCompany instance = new CardCompany();
+
+    private CardCompany() {};
+
+    public static CardCompany getInstance()
+    {
+        return instance;
+    }
+
+    public Card createCard()
+    {
+        Card card = new Card();
+        return card;
+    }
 }
