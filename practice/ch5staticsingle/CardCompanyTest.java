6d079d402ef7185a6043080bef0dd1778ac0c42a
hws522 <sraphic@kakao.com>
2020-10-28 17:26:51 +0900

    finish

--- practice/ch5staticsingle/CardCompanyTest.java
+++ practice/ch5staticsingle/CardCompanyTest.java
@@ -4,7 +4,7 @@ public class CardCompanyTest {
     public static void main(String[] args) {
         CardCompany company = CardCompany.getInstance(); // 싱글톤패턴
 
-        Card myCard = company.createCard(); //메서드에서 Car 생성.
+        Card myCard = company.createCard(); //메서드에서 Card 생성.
         Card yourCard = company.createCard();
 
         System.out.println(myCard.getCardNumber()); //10001
