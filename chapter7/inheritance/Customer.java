3577c5243a0ff9ff41b2dad99467f80a3b8a0f2f
hws522 <sraphic@kakao.com>
2020-11-24 19:26:56 +0900

    상속에서 클래스 생성 과정 및 형 변환.

--- chapter7/inheritance/Customer.java
+++ chapter7/inheritance/Customer.java
@@ -1,57 +1,55 @@
 package chapter7.inheritance;
 
 public class Customer {
-    
-    protected int customerID; //private 는 상속되더라도 기능을 쓸 수 없음. 하위클래스는 기능은 쓸 수 있는 protected 를 쓴다.
+
+    protected int customerID; // private 는 상속되더라도 기능을 쓸 수 없음. 하위클래스는 기능은 쓸 수 있는 protected 를 쓴다.
     protected String customerName;
     protected String customerGrade;
     int bonusPoint;
     double bonusRatio;
 
-    public Customer() //기본 생성자.
-    {
-        customerGrade = "SILVER";
-        bonusRatio = 0.01;
+    // public Customer() //기본 생성자.
+    // {
+    // customerGrade = "SILVER";
+    // bonusRatio = 0.01;
+    // }
+
+    public Customer(int customerID, String customerName) {
+        this.customerID = customerID;
+        this.customerName = customerName;
     }
 
-    public int calcPrice(int price) //가격계산. 돈을 받았을 때 지불할 금액을 반환.
+    public int calcPrice(int price) // 가격계산. 돈을 받았을 때 지불할 금액을 반환.
     {
         bonusPoint += price * bonusRatio;
         return price;
     }
 
-    public String showCustomerInfo()
-    {
+    public String showCustomerInfo() {
         return customerName + "님의 등급은 " + customerGrade + " 이며, 적립된 보너스 포인트는 " + bonusPoint + "점 입니다.";
     }
 
-    public int getCustomerID()
-    {
+    public int getCustomerID() {
         return customerID;
     }
 
-    public void setCustomerID(int customerID)
-    {
+    public void setCustomerID(int customerID) {
         this.customerID = customerID;
     }
 
-    public String getCustomerName()
-    {
+    public String getCustomerName() {
         return customerName;
     }
 
-    public void setCustomerName(String customerName)
-    {
+    public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
 
-    public String getCustomerGrade()
-    {
+    public String getCustomerGrade() {
         return customerGrade;
     }
 
-    public void setCoustomerGrade(String customerGrade)
-    {
+    public void setCoustomerGrade(String customerGrade) {
         this.customerGrade = customerGrade;
     }
 }
