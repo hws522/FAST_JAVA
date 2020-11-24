8fa9d2a73662d1465cc3ef43dbdbf3c2959ec627
hws522 <sraphic@kakao.com>
2020-11-25 02:54:59 +0900

    템플릿메서드 연습

---
+++ practice/ch8template/Car.java
@@ -0,0 +1,19 @@
+package practice.ch8template;
+
+public abstract class Car {
+
+    public abstract void start();
+
+    public abstract void drive();
+
+    public abstract void stop();
+
+    public abstract void turnOff();
+
+    final public void run() {
+        start();
+        drive();
+        stop();
+        turnOff();
+    }
+}
