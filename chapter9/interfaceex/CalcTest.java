60c0ef4aa1f42bccdc01d3af650cda28eb7253d6
hws522 <sraphic@kakao.com>
2020-12-30 14:12:18 +0900

    default method, private method

--- chapter9/interfaceex/CalcTest.java
+++ chapter9/interfaceex/CalcTest.java
@@ -10,6 +10,11 @@ public class CalcTest {
         System.out.println(calc.substract(n1, n2));
         System.out.println(calc.times(n1, n2));
         System.out.println(calc.divide(n1, n2));
-        calc.showInfo();
+
+        calc.description();
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int sum = Calc.total(arr); // static 으로 생성해놓으면, 인스턴스 생성과 상관없이 가져다 쓸 수 있음.
+        System.out.println(sum);
     }
 }
