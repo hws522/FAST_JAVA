26230178d1db7910533776d3ff126cbde714e2bf
hws522 <sraphic@kakao.com>
2020-12-31 23:12:00 +0900

    Comparable, Comparator

---
+++ chapter11/collection/treeset/Member.java
@@ -0,0 +1,58 @@
+package chapter11.collection.treeset;
+
+public class Member implements Comparable<Member> { // 다른 객체를 비교할 경우, 비교방법을 구현해야함. Comparable or Comparator
+
+    private int memberId;
+    private String memberName;
+
+    public Member() {
+    }
+
+    public Member(int memberId, String memberName) {
+        this.memberId = memberId;
+        this.memberName = memberName;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String toString() {
+        return memberName + "회원님의 아이디는 " + memberId + "입니다.";
+    }
+
+    @Override
+    public boolean equals(Object obj) { // memberId가 같을 때 같은 값이라는 것이 정의되어 있지 않으므로, 오버라이딩을 통해 정의.
+        if (obj instanceof Member) {
+            Member member = (Member) obj;
+            return (this.memberId == member.memberId);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return memberId;
+    }
+
+    @Override
+    public int compareTo(Member member) { // 매개변수와 나를 비교하면 됨.
+
+        // return this.memberName.compareTo(member.getMemberName()); // 이름 순으로 정렬 하고 싶을
+        // 경우.
+        return (this.memberId - member.memberId);
+    }
+}
