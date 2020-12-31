f2ce3838dbfccf2052ecf11cd1b4c15999433401
hws522 <sraphic@kakao.com>
2020-12-31 22:45:31 +0900

    Set interface(HashSet)

--- chapter11/collection/set/Member.java
+++ chapter11/collection/set/Member.java
@@ -1,4 +1,4 @@
-package chapter11.collection;
+package chapter11.collection.set;
 
 public class Member {
 
@@ -33,4 +33,18 @@ public class Member {
         return memberName + "회원님의 아이디는 " + memberId + "입니다.";
     }
 
+    @Override
+    public boolean equals(Object obj) { // memberId가 같을 때 같은 값이라는 것이 정의되어 있지 않으므로, 오버라이딩을 통해 정의.
+        if (obj instanceof Member) {
+            Member member = (Member) obj;
+            return (this.memberId == member.memberId);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return memberId;
+    }
 }
