5174b7405dc9872757b60cca68ec22c170881ebc
hws522 <sraphic@kakao.com>
2021-01-17 19:08:16 +0900

    stream

---
+++ chapter12/stream/ReduceTest.java
@@ -0,0 +1,37 @@
+package chapter12.stream;
+
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+
+public class ReduceTest {
+    
+    public static void main(String[] args) {
+        
+        String[] greetings = {"안녕하세요!!", "Hello", "Good Morning", "반갑습니다"};
+
+        System.out.println(Arrays.stream(greetings).reduce("", (s1, s2) -> {
+            if(s1.getBytes().length >= s2.getBytes().length){
+                return s1;
+            }
+            else{
+                return s2;
+            }
+        }));
+
+        System.out.println(Arrays.stream(greetings).reduce(new CompareString()).get());
+    }
+}
+
+class CompareString implements BinaryOperator<String>{
+
+    @Override
+    public String apply(String t, String u) {
+        if(t.getBytes().length >= u.getBytes().length){
+            return t;
+        }
+        else{
+            return u;
+        }
+    }
+    
+}
