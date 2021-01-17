729cbf2c4a4f9b1774cbc7ab77d2845c48b7200e
hws522 <sraphic@kakao.com>
2021-01-17 15:43:10 +0900

    lamda

---
+++ chapter12/lambda/TestLambda.java
@@ -0,0 +1,25 @@
+package chapter12.lambda;
+
+interface PrintString{
+    void showString(String str);
+}
+
+public class TestLambda {
+    public static void main(String[] args) {
+        PrintString lambdaStr = s -> System.out.println(s);
+        lambdaStr.showString("test");
+
+        showMyString(lambdaStr);
+
+        PrintString test = returnString();
+        test.showString("test3");
+    }
+
+    public static void showMyString(PrintString p){
+        p.showString("test2");
+    }
+
+    public static PrintString returnString(){
+        return s -> System.out.println(s + "!!!");
+    }
+}
--- chapter12/lambda/TestStringConcat.java
+++ chapter12/lambda/TestStringConcat.java
@@ -1,5 +1,7 @@
 package chapter12.lambda;
 
+import First.hello.HelloJava;
+
 public class TestStringConcat {
     public static void main(String[] args) {
         
@@ -8,5 +10,15 @@ public class TestStringConcat {
 
         StringConcat concat = (s, v) -> System.out.println(s + ',' + v);
         concat.makeString("hello", "lambda!");
+
+        StringConcat concat2 = new StringConcat(){
+
+            @Override
+            public void makeString(String s1, String s2) { // 람다식으로 구현을 하지만 사실 익명내부클래스 생성됨.
+                System.out.println(s1 + ',' + s2);
+            }
+        };
+
+        concat2.makeString("hello", "String");
     }
 }
